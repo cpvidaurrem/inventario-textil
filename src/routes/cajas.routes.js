136d4b5a099ec49b15b6b8055b37a331b55c5dce
cpvidaurrem <kristian2vidaurre@gmail.com>
2026-03-29 00:41:51 -0400

    consultar inventario y manejar salidas

--- src/routes/cajas.routes.js
+++ src/routes/cajas.routes.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { crearCaja } = require("../controllers/cajas.controller");
+const {
+  crearCaja,
+  obtenerCajas,
+  buscarCajas,
+  registrarSalida,
+  inventarioResumen,
+} = require("../controllers/cajas.controller");
 
 const router = Router();
 
 router.post("/cajas", crearCaja);
+router.get("/cajas", obtenerCajas);
+router.get("/cajas/buscar", buscarCajas);
+router.post("/salidas", registrarSalida);
+router.get("/inventario", inventarioResumen);
 
 module.exports = router;
